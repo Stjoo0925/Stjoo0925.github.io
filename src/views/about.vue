b85a64dd0fca56274559b3cddd9c9a225c4f62f0
Stjoo0925 <stjoo0925@gmail.com>
2024-09-15 13:26:29 +0900

    fix : view fix

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,22 +1,8 @@
-<script setup>
-import { onMounted } from "vue";
-import { useLoadingStore } from "../stores/loadingStore";
-import Loading from "@/components/loading.vue";
-
-const loadingStore = useLoadingStore();
-
-onMounted(() => {
-  loadingStore.startLoading();
-  setTimeout(() => {
-    loadingStore.stopLoading();
-  }, 500);
-});
-</script>
+<script setup></script>
 
 <template>
   <div class="about-container">
-    <Loading />
-    <div v-if="!loadingStore.isLoading">자기소개 페이지 입니다.</div>
+    <div>자기소개 페이지 입니다.</div>
   </div>
 </template>
 
--- src/views/portfolio.vue
+++ src/views/portfolio.vue
@@ -1,22 +1,8 @@
-<script setup>
-import { onMounted } from "vue";
-import { useLoadingStore } from "../stores/loadingStore";
-import Loading from "@/components/loading.vue";
-
-const loadingStore = useLoadingStore();
-
-onMounted(() => {
-  loadingStore.startLoading();
-  setTimeout(() => {
-    loadingStore.stopLoading();
-  }, 500);
-});
-</script>
+<script setup></script>
 
 <template>
   <div>
-    <Loading />
-    <div v-if="!loadingStore.isLoading">포트폴리오 페이지 입니다.</div>
+    <div>포트폴리오 페이지 입니다.</div>
   </div>
 </template>
 
