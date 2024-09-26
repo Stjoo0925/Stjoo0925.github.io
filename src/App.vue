242a1cbefddcef1666b0eb8472f159ebfd8a8c82
Stjoo0925 <stjoo0925@gmail.com>
2024-09-26 22:17:06 +0900

    build: layout fixed

--- src/App.vue
+++ src/App.vue
@@ -24,27 +24,29 @@ import Footer from "./components/footer.vue";
 .app-container {
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 100vh; /* 전체 높이를 100vh로 설정 */
   width: 100%;
   overflow: hidden;
   background-color: var(--main-bg-color);
 }
 
 .app-header {
-  height: 8vh; /* 헤더의 높이를 명확하게 지정 */
+  height: 8vh; /* 헤더의 높이를 지정 */
   width: 100%;
+  flex-shrink: 0; /* 고정된 높이를 유지 */
 }
 
 .app-section {
+  flex: 1; /* 남은 공간을 차지하도록 설정 */
   position: relative;
-  height: 87vh; /* 섹션의 높이를 명확하게 지정 */
   width: 100%;
   overflow: hidden;
-  z-index: 1; /* 섹션의 z-index 설정 */
+  z-index: 1;
 }
 
 .app-footer {
-  height: 5vh; /* 푸터의 높이를 명확하게 지정 */
+  height: 5vh; /* 푸터의 높이를 지정 */
   width: 100%;
+  flex-shrink: 0; /* 고정된 높이를 유지 */
 }
 </style>
