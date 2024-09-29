9f9e2c866bfaa13ed005e36555b382f2e892e340
Stjoo0925 <stjoo0925@gmail.com>
2024-09-30 01:16:02 +0900

    build: createWebHashHistory update

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const About = () => import(/* webpackPrefetch: true */ "@/views/about.vue");
 const TechSkills = () => import(/* webpackPrefetch: true */ "@/views/techSkills.vue");
@@ -29,7 +29,7 @@ const routeOrder = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
